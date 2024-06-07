e7794c228c779aabb9f762b51792c028b8217f26
Gabriel Guimaraes <gabrielplug32@gmail.com>
2024-06-07 14:52:15 -0300

    adding userapi to the project

---
+++ userapi/src/test/java/com/br/userapi/userapi/UserapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.userapi.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
